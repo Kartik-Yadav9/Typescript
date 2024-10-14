cc84c5e0bbb12ca55c836f3522d37c938a5ee132
kartik <kartikyadav0999@gmail.com>
2024-10-15 03:03:44 +0530

    any

--- Typescript_First/testing.js
+++ Typescript_First/testing.js
@@ -1,6 +1,25 @@
 "use strict";
+//-------------------------------------------------------NUMBER-------------------------------------------------
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log('helllo');
-var age = 25;
+var age = 25; //let can be used in block scope({}) that's why using it
+// age= "kartik"                                   //it will show error because number is assigned not string
 console.log();
-//let can be used in block scope({}) that's why using it
+//-------------------------------------------------------STRING--------------------------------------------------
+var str = "kartik";
+// str.toUpperCase()                           //will give all methods related to string
+//------------------------------------------------------BOOLEAN---------------------------------------------------
+var bool = false;
+// bool.valueOf()                            //will provide methods
+//--------------------------------------------------ANY-------------------------------------------------------------
+// function anyKeyword(){
+//     return "hi"
+// }
+// let ans =anyKeyword()
+// console.log(ans);
+// let ans                                 //declaring over here it is using any keyword
+var ans; //here declared string means, u cannot return number or boolean
+function anyKeyword() {
+    return "hi";
+}
+ans = anyKeyword();
+console.log(ans);
--- Typescript_First/testing.ts
+++ Typescript_First/testing.ts
@@ -1,8 +1,69 @@
-console.log('helllo');
+//-------------------------------------------------------NUMBER-------------------------------------------------
 
-const age=25
+let age:number = 25                             //let can be used in block scope({}) that's why using it
+
+// age= "kartik"                                   //it will show error because number is assigned not string
 console.log();
 
-export {}                                     //Adding export {} turns the file into a module, scoping all its variables to the file itself
-                                              //let can be used in block scope({}) that's why using it
+export {}                                       //Adding export {} turns the file into a module, scoping all its variables to the file itself
+                                             
+
+
+
+
+
+
+
+//-------------------------------------------------------STRING--------------------------------------------------
+
+let str: string = "kartik"
+
+// str.toUpperCase()                           //will give all methods related to string
+
+
+
+
+
+
+
+
+
+
+//------------------------------------------------------BOOLEAN---------------------------------------------------
+
+let bool: boolean = false
+
+// bool.valueOf()                            //will provide methods
+
+
+
+
+
+
+
+
+//--------------------------------------------------ANY-------------------------------------------------------------
+
+// function anyKeyword(){
+//     return "hi"
+// }
+
+// let ans =anyKeyword()
+// console.log(ans);
+
+
+
+
+
+// let ans                                 //declaring over here it is using any keyword
+
+let ans: string                            //here declared string means, u cannot return number or boolean
+
+function anyKeyword(){
+    return "hi"
+}
+                                            
+ ans =anyKeyword()
+console.log(ans);
+
 
