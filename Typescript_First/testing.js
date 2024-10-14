0581323d511bc6d0249eb06bbe253165ce9c9209
kartik <kartikyadav0999@gmail.com>
2024-10-15 01:56:40 +0530

    les

--- Typescript_First/testing.js
+++ Typescript_First/testing.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('helllo');
 var age = 25;
-console.log(age);
+console.log();
+//let can be used in block scope({}) that's why using it
--- Typescript_First/testing.ts
+++ Typescript_First/testing.ts
@@ -1,5 +1,8 @@
 console.log('helllo');
 
-let age=25
-console.log(age);
+const age=25
+console.log();
+
+export {}                                     //Adding export {} turns the file into a module, scoping all its variables to the file itself
+                                              //let can be used in block scope({}) that's why using it
 
