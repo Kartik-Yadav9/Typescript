3c3eb14029975ffb3aebc16db106d1a4b1faf12a
kartik <kartikyadav0999@gmail.com>
2026-05-29 13:24:00 +0530

    basic

---
+++ basic/Basic.js
@@ -0,0 +1,10 @@
+"use strict";
+function testing12(name) {
+    return 24;
+}
+console.log(testing12("number"));
+//=================================================
+function testing2(name) {
+    return `24, ${name}`;
+}
+console.log(testing2("hii"));
---
+++ basic/Basic.ts
@@ -0,0 +1,18 @@
+function practice(): number {
+  return 24
+}
+
+console.log(practice());
+
+
+
+
+
+
+//=================================================
+
+function testing2(name: string): string {
+  return `24, ${name}`;
+}
+
+console.log(testing2("byeee"));
