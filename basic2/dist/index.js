66322c88c1c745d10eaa28c70c91059cf3c12e3c
kartik <kartikyadav0999@gmail.com>
2026-06-10 18:07:46 +0530

    assignment

---
+++ basic2/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function first(name) {
+    return `hi ${name}`;
+}
+console.log(first("kartik"));
+//# sourceMappingURL=index.js.map
+
---
+++ basic2/src/index.ts
@@ -0,0 +1,8 @@
+function first(name: string): string{
+return `hi ${name}`
+}
+
+console.log(
+ first("kartik"))
+
+ let cup= Math.random() > 0.5 ? 1 : 0
